25fa348538e8f79a7edd0dccfb005323f3ee380e
Bishal Ghosh <bishal28ghosh@gmail.com>
2026-04-02 21:20:00 +0530

    Add files via upload

---
+++ pro04.py
@@ -0,0 +1,10 @@
+#Accept two numbers and print the greatest between them
+
+num1 = int(input("give me the number "))
+num2 = int(input("give me the number "))
+
+if num1>num2:
+    print(f"{num1} is greater then {num2}")
+
+else:
+    print(f"{num2} is greater then {num1}")
---
+++ pro05.py
@@ -0,0 +1,10 @@
+gender = input("please tell me ur gender as character(M or F)")
+
+if gender == 'M' or gender == 'm':
+    print("Good moring sir")
+
+elif gender =="F" or gender =='f' :
+    print("Good moring mam")
+
+else:
+    print("unidentified genderf")    
